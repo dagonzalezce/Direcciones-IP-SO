6466fcdf472db214aa1fa2f111e4ea956a3b5ef1
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2020-03-23 00:13:19 -0500

    calculando dirección de red de una ip

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function calcular(){
 
 		numListDireccionIP1 = dividirStringEnOctetos(inputDireccionIP1.value);
 		numListMascaraDeRed = dividirStringEnOctetos(inputMascaraDeRed.value);
+		numListDireccionIP1 = enBinario(numListDireccionIP1);
+		numListMascaraDeRed = enBinario(numListMascaraDeRed);
+		console.log(numListDireccionIP1);
 
 		if (inputDireccionIP2.value.length == 0){
 			calcularUnaDireccion(numListDireccionIP1,numListMascaraDeRed);
@@ -28,20 +31,50 @@ function calcular(){
 	}
 }
 
+
 function calcularUnaDireccion(numListDireccionIP1,numListMascaraDeRed){
 	var f;
-	
+	var contador = 0;
 	var direccion_red = new Array(numListDireccionIP1.length);
 	for(f=0; f<numListDireccionIP1.length; f++){
-		if (numListDireccionIP1[f]==1 && numListMascaraDeRed[f]==1){
-			direccion_red[f]=1;
+
+		if (numListMascaraDeRed[f]=="1"){
+			contador++;
+		}
+
+		if (numListDireccionIP1[f]=="1" && numListMascaraDeRed[f]=="1"){
+			direccion_red[f]="1";
 		}
 		else{
-			direccion_red[f]=0;
+
+			direccion_red[f]="0";
 		}
 	}
 
+	var new_direccion_red = direccion_red.join("");
+	var direccion_red_decimal = new Array();
+
+	//será mejor si lo hago en un for ????
+	
+	direccion_red_decimal.push(new_direccion_red.substring(0,8));
+	direccion_red_decimal.push(new_direccion_red.substring(8,16));
+	direccion_red_decimal.push(new_direccion_red.substring(16,24));
+	direccion_red_decimal.push(new_direccion_red.substring(24,32));
+
+
+	var direccion_red_decimal2 = new Array();
+
+	for(f=0; f<direccion_red_decimal.length; f++){
+		var dir_decimal = parseInt(direccion_red_decimal[f], 2);
+		direccion_red_decimal2.push(dir_decimal);
+	}
+
+	var direccion_final = 'Dirección de red: ' + direccion_red_decimal2.join('.').concat('/' + contador);
+
+	document.getElementById("unaip").innerText=direccion_final;
+
 	$('#modalUnaDireccionIP').modal('show');
+
 }
 
 function calcularDosDirecciones(numListDireccionIP1,numListMascaraDeRed,numListDireccionIP2){
@@ -57,13 +90,13 @@ function enBinario(array_dir){
 		numString2 = numString2 + ("0").repeat(8-numString.length).concat(numString);
 
 	}
-	return numString2.split("").map(function (x) { return parseInt(x, 10); });
+	return numString2.split("");
 }
 
-// Devuelve un array con los cuatro números en decimal (0-255) que hay en un string separados por puntos 
+// Devuelve un array con los cuatro números en decimal (0-255) que hay en un string separados por puntos
 function dividirStringEnOctetos(str){
 	var res = str.split(".");
-	res = res.map(function (x) { 
+	res = res.map(function (x) {
 				  var num = parseInt(x, 10);
 				  if(isNaN(num)) throw "Entradas no válidas";
 				  if(num>255) throw "Cada número debe estar entre 0 y 255";
@@ -72,4 +105,3 @@ function dividirStringEnOctetos(str){
 	if(res.length != 4) throw "Debes ingresar cuatro números por dirección IP / Máscara de Red"
 	return res;
 }
-
